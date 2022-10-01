b1be462850121245ffb4ffd9065e1365ed2e4d2a
Ivan Adrianto <ivanadrianto64@gmail.com>
2022-10-01 20:02:34 +0700

    add animation in landing page

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ if (pathname === "/" || pathname === "/index.html") {
   homeButton.className = "nav-item";
   portfolioButton.className = "nav-item active";
 }
+
+function scrollTrigger(selector) {
+  let els = document.querySelectorAll(selector);
+  els = Array.from(els);
+  els.forEach((el) => {
+    addObserver(el);
+  });
+}
+function addObserver(el) {
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+        observer.unobserve(entry.target);
+      }
+    });
+  });
+  observer.observe(el);
+}
+scrollTrigger("#expertise");
+scrollTrigger("#working-exp");
+scrollTrigger("#education")
+scrollTrigger("#portfolio-home")
